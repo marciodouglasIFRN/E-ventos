6e09d84d6a33858cfefe9afe1fb55d0d3a703a10
Márcio Douglas <marciodouglasxavier@gmail.com>
2018-12-10 22:39:30 -0300

    Qrcode stático

--- promotor/urls.py
+++ promotor/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from .views import promoter
-
+from .views import qrcode
 urlpatterns = [
     path('promoter/', promoter, name="page-promoter"),
+    path('qrcode/', qrcode, name="page-qrcode"),
 ]
--- promotor/views.py
+++ promotor/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
 
+import pyqrcode
 # Create your views here.
 def promoter(request):
 	return render(request, 'promoter.html')
+
+def qrcode(request):
+	url = pyqrcode.create('https://github.com/marciodouglasIFRN/E-ventos')
+	url.png('statics/imgs/code.png', scale=8)
+	return render(request,'qrcode.html')
