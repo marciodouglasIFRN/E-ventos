183d947f74125c6e81099e476635720e10c64471
Matheus Kaio <matheuskaio2000@gmail.com>
2019-04-05 15:05:45 -0300

    Alterando ip para teste local mais uma vez

--- gerenciamentoevento/settings.py
+++ gerenciamentoevento/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '@7#_(cpavm_d2$ima0qqmk0$9xf0nn(s8in(=9tcw8t9ys@f$o'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['192.168.1.106', '10.0.0.117']
+ALLOWED_HOSTS = ['192.168.1.113', '192.168.1.106', '10.0.0.117', 'localhost', '127.0.0.1']
 
 
 # Application definition
